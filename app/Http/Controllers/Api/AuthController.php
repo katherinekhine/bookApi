c35e00551f8dd11e25e4387d841dd310981f95d0
katherinekhine <khaingkatherine@gmail.com>
2024-11-30 12:08:47 +0630

    update api.php and add AuthController

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:4',
+        ]);
+        $user = User::create($data);
+        return response()->json($user, 200);
+    }
+}
